be0094f56d3f604fdf69ba27c1db0fffd6af5d0d
thijsjls <thijs.jls@gmail.com>
2023-12-04 17:44:19 +0100

    add timing to template

--- day_0/solution.py
+++ day_0/solution.py
@@ -1,4 +1,17 @@
 """Day 4 solution."""
+import time
+
+
+def timeit(f):
+
+    def timed(*args, **kw):
+        ts = time.time()
+        result = f(*args, **kw)
+        te = time.time()
+        print(f"func:{f.__name__} took: {te-ts} sec")
+        return result
+
+    return timed
 
 
 def read_input(test: bool = False) -> list[str]:
@@ -11,12 +24,14 @@ def read_input(test: bool = False) -> list[str]:
         return lines
 
 
+@timeit
 def part_1(lines: list[str]) -> int:
     """Solution to part 1."""
     solution = 0
     return solution
 
 
+@timeit
 def part_2(lines: list[str]) -> int:
     """Solution to part 2."""
     solution = 0
